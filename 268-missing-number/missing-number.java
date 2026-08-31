1698de98be7950d1bcafa94338e79022d0ec7887
Mrunmayee Mhatre <mrunmayeepmhatre21@gmail.com>
2026-08-31 20:23:35 +0530

    Time: 1 ms (29.60%) | Memory: 47.3 MB (49.03%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int i=0;
+        while(i<n){
+            int correct=nums[i];
+            if(correct<n && nums[i] != nums[correct]){
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct]=temp;
+            }else{
+                i++;
+            }
+        }
+        for(i=0;i<n;i++){
+            if(nums[i]!=i)return i;
+        }
+        return n;
+    }
+}
